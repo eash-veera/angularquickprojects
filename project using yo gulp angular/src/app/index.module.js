66f56dd2ea08cc95d3446538b9dc4b92ce83d08a
Eshwaran Veerabahu <eshwaranveerabahu.biz@gmail.com>
2017-09-05 14:49:41 +0530

    yeoman generated angular gulp project added to the structure

---
+++ project using yo gulp angular/src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('projectUsingYoGulpAngular', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ui.router', 'ngMaterial', 'toastr']);
+
+})();
